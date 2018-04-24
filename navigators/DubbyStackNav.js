c9bb90dab72ca280471ab25123514fca8c3a859e
Gareth Field <field.gareth@gmail.com>
2018-04-24 12:40:09 -0400

    plz no

--- navigators/DubbyStackNav.js
+++ navigators/DubbyStackNav.js
@@ -1,6 +1,6 @@
 import { StackNavigator } from "react-navigation";
 import DubbyNavRoutes from "../routes/DubbyNavRoutes";
 
-const DubbyStackNav = StackNavigator(DubbyNavRoutes);
+const DubbyStackNav = StackNavigator(DubbyNavRoutes, { headerMode: "screen" });
 
 export default DubbyStackNav;

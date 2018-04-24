a1c12c0356d84b322a362c941d92f5deaa3a666a
Gareth Field <field.gareth@gmail.com>
2018-04-24 12:35:48 -0400

    nooo

--- reducers/counterReducer.js
+++ reducers/counterReducer.js
@@ -1,1 +1,16 @@
-kjhlkjh;
+const initialCounterState = { count: 0 };
+
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      console.log("Increment called");
+      return { ...state, count: count + 1 };
+    case "DECREMENT":
+      console.log("Decrement called");
+      return { ...state, count: count - 1 };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;

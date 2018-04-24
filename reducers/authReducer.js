60ee0c1a00088d51667f7b8a390c33bc818981da
Gareth Field <field.gareth@gmail.com>
2018-04-24 12:35:09 -0400

    noo

--- reducers/authReducer.js
+++ reducers/authReducer.js
@@ -1,1 +1,14 @@
-lkjhblkj;
+const initialAuthState = { isLoggedIn: false };
+
+const auth = (state = initialAuthState, action) => {
+  switch (action.type) {
+    case "Login":
+      return { ...state, isLoggedIn: true };
+    case "Logout":
+      return { ...state, isLoggedIn: false };
+    default:
+      return state;
+  }
+};
+
+export default auth;

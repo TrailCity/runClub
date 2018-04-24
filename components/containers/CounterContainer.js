8a5d9c2574da8e7e4bdebb9cbed5b61805c74523
Gareth Field <field.gareth@gmail.com>
2018-04-24 12:38:32 -0400

    no why

--- components/containers/CounterContainer.js
+++ components/containers/CounterContainer.js
@@ -1,1 +1,20 @@
-mjhglkjhl;
+import { connect } from "react-redux";
+import { INCREMENT, DECREMENT } from "../../actions/creators";
+import Counter from "../Counter";
+
+const mapStateToProps = state => ({
+  count: state.counter.count
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increment: () => {
+      dispatch(INCREMENT);
+    },
+    decrement: () => {
+      dispatch(DECREMENT);
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
